dac69479202d65a7e242fc817a05d8d20c0350cc
Tajbhoraniya <bhoraniyatajtaj.com>
2025-01-19 23:48:18 -0500

    test opreation

---
+++ test/test_operation.py
@@ -0,0 +1,12 @@
+from src.mathsoperation import add,sub
+
+def test_add():
+    assert add(2,3)==5
+    assert add(1,6)==7
+    
+
+def test_sub():
+    assert add(2,3)==-1
+    assert add(1,6)==-5
+    assert add(3,1)==2
+
